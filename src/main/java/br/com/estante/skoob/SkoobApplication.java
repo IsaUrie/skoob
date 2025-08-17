e18f6faf65af2d5443939323d62a5224a8d88b1b
IsaUrie <88585867+IsaUrie@users.noreply.github.com>
2025-08-17 19:04:41 -0300

    first commit

---
+++ src/main/java/br/com/estante/skoob/SkoobApplication.java
@@ -0,0 +1,13 @@
+package br.com.estante.skoob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkoobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkoobApplication.class, args);
+	}
+
+}
